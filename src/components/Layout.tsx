54bd47d7792b11915a548d8a6bf2882ffbbd06a8
Muhammad Taufiq Febrianto <taufiqisfebrianto06@gmail.com>
2022-10-04 21:18:30 +0700

    Update isOpen di dalam useEffect

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import type { Route } from 'nextjs-routes';
 import type { ReactNode } from 'react';
+import { useEffect } from 'react';
 import Nav from './Nav';
 import SignInDialog, { useSignInDialogStore } from './SignInDialog';
 
@@ -14,25 +15,27 @@ const Layout = (props: Props) => {
 	const session = useSession();
 	const signInDialogStore = useSignInDialogStore();
 
-	const protectedRoutes: Array<Route['pathname']> = [
-		'/submit',
-		'/playlists/[id]/edit',
-		'/me/playlists'
-	];
+	useEffect(() => {
+		const protectedRoutes: Array<Route['pathname']> = [
+			'/submit',
+			'/playlists/[id]/edit',
+			'/me/playlists'
+		];
 
-	let children = props.children;
-	if (
-		session.status === 'unauthenticated' &&
-		protectedRoutes.includes(router.pathname)
-	) {
-		children = null;
-		signInDialogStore.setIsOpen(true);
-	}
+		if (
+			session.status === 'unauthenticated' &&
+			protectedRoutes.includes(router.pathname)
+		) {
+			signInDialogStore.setIsOpen(true);
+		}
+	}, [router.pathname, session.status, signInDialogStore]);
 
 	return (
 		<>
 			<Nav />
-			<main className="mx-auto mt-32 mb-10 max-w-6xl md:mt-28">{children}</main>
+			<main className="mx-auto mt-32 mb-10 max-w-6xl md:mt-28">
+				{session.status === 'unauthenticated' ? null : props.children}
+			</main>
 			<SignInDialog />
 		</>
 	);
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,11 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import { getBaseUrl } from '../utils/general';
 
 export default function Document() {
 	return (
 		<Html>
 			<link
 				rel="preload"
-				href={`${getBaseUrl()}/fonts/Inter-VariableFont_slnt,wght.woff2`}
+				href={`/fonts/Inter-VariableFont_slnt,wght.woff2`}
 				as="font"
 				type="font/woff2"
 				crossOrigin="anonymous"
